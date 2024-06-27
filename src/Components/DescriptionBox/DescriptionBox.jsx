646e4e1a562c2b092efec8f146224a05b07e995c
Amandeep Kaur <107187322+Amandeepkaur1804@users.noreply.github.com>
2024-06-27 22:02:13 +0530

    Files

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+ const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e-commerce website is an online platform where users can buy and sell goods and services. It provides a virtual shopping experience, allowing customers to browse products, add them to a cart, and make secure online payments. E-commerce websites often feature a wide range of products, organized into categories for easy navigation. They also provide detailed product descriptions, reviews, and ratings to help customers make informed purchasing decisions. With the convenience of shopping from anywhere and at any time, e-commerce websites have become increasingly popular and essential in today's digital age.</p>
+            <p>An e-commerce website is a digital platform where businesses and consumers can engage in online transactions. It offers a virtual shopping experience, allowing customers to browse a wide range of products from the comfort of their homes. These websites feature organized product categories, detailed descriptions, images, customer reviews, and ratings to assist shoppers in making informed decisions. Additionally, e-commerce platforms provide secure payment gateways, ensuring the safety of online transactions. With the convenience of 24/7 accessibility and the ability to shop from anywhere in the world, e-commerce websites have revolutionized the way people shop, making it easier and more convenient than ever to purchase goods and services online.</p>
+        </div>
+    </div>
+  )
+}
+export default DescriptionBox
